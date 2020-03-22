43e11bab8192066a6c84f29aabb7c38d1c52414f
johnflacon122 <johnflacon122@gmail.com>
2020-03-22 12:50:48 +1100

    Add project files.

---
+++ TestingDemo/TestClass.cs
@@ -0,0 +1,43 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestingDemo
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.news.com.au/";
+
+            //driver.FindElement(By.Id("show-search")).Click();
+            driver.FindElement(By.Id("search-input")).Click();
+
+            driver.FindElement(By.Id("search-input")).SendKeys("coronavirus");
+
+            driver.FindElement(By.Id("show-search")).Click();
+
+            Thread.Sleep(2000);
+
+            IList<IWebElement> list = driver.FindElements(By.XPath("//div[@class='group search-results']//div//a//h5"));
+
+            System.Console.WriteLine(list.Count);
+
+            foreach(IWebElement el in list)
+            {
+                System.Console.WriteLine(el.Text);
+            }
+
+            driver.Quit();
+        }
+    }
+}
